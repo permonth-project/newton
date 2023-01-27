ca6e51aef0b96691308b1e4a08af10da1a218a7a
jiwooshim <jiwooshim96@gmail.com>
2023-01-27 08:04:20 +0800

    add fastapi backend

---
+++ app/crud.py
@@ -0,0 +1,47 @@
+from sqlalchemy.orm import Session
+import models
+import pandas as pd
+
+
+def get_prices(db: Session, skip: int = 0, limit: int = 100):
+    # db.execute()
+    return db.query(models.Listing).limit(limit).all()
+
+
+def get_all_prices(db: Session, skip: int = 0, limit: int = 100):
+    ## get prices for each listing on the enrtire database
+    res = db.execute(f"""
+        SELECT listing.*, product.model_name, product.color, product.capacity, product.current_  
+        FROM (SELECT post_id, result_price, product_id FROM mydb.listing) as listing, 
+        (SELECT id, model_name, color, capacity, current_ FROM mydb.product) as product 
+        WHERE listing.product_id=product.id 
+        AND product.current_=1;
+    """)
+
+    df = pd.DataFrame(list(res))
+    return df.set_index('post_id').to_dict(orient='index')
+
+
+
+def get_avg_prices(db: Session, skip: int = 0, limit: int = 100):
+    ## get prices for each listing on the enrtire database
+    res = db.execute(f"""
+        SELECT listing.*, product.model_name, product.color, product.capacity, product.current_  
+        FROM (SELECT post_id, result_date, result_price, product_id FROM mydb.listing) as listing, 
+        (SELECT id, model_name, color, capacity, current_ FROM mydb.product) as product 
+        WHERE listing.product_id=product.id 
+        AND product.current_=1;
+    """)
+
+    df = pd.DataFrame(list(res))
+
+    df_res = pd.DataFrame()
+    df_group = df[['product_id', 'result_price', 'model_name', 'capacity', 'color']].astype(
+        {'product_id': int, 'result_price': float}).groupby(by=['product_id', 'model_name', 'capacity', 'color'])
+    df_res.loc[:, 'price_avg'] = df_group.mean()
+    df_res.loc[:, 'price_high'] = df_group.max()
+    df_res.loc[:, 'price_low'] = df_group.min()
+    df_res = df_res.reset_index()
+
+    return df_res.to_dict(orient='records')
+
---
+++ app/main.py
@@ -0,0 +1,43 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+import crud
+import models
+import schemas
+from database import SessionLocal, engine
+import uvicorn
+import pandas as pd
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# @app.get("/all_prices/", response_model=list[schemas.AllPrices])
+@app.get("/get-all/")
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    all_prices = crud.get_all_prices(db, skip=skip, limit=limit)
+    # all_prices = list(all_prices)
+    # all_prices = pd.DataFrame(all_prices).to_dict(orient="list")
+
+    return all_prices
+
+
+@app.get("/get-avg/")
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    avg_prices = crud.get_avg_prices(db, skip=skip, limit=limit)
+
+    return avg_prices
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
