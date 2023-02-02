508e2c860bef174d1351b8693f76c7b1ea70daa5
jiwooshim <jiwooshim96@gmail.com>
2023-02-02 13:22:01 +0800

    add graph dummy page

---
+++ frontend/src/components/dashboard/GraphLine.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import axios from "axios";
+
+
+const GraphBar = () => {
+    const [data, setData] = useState();
+    const [isFetched, setIsFetched] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                `http://newton.permonth.tech/get-avg/?product_id=466`,
+            );
+
+            setData(result.data);
+            setIsFetched(true)
+        };
+
+        fetchData();
+    }, []);
+
+    const options = {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: ''
+        },
+        series: [
+            {
+                data: data?.map(d => d.price_avg)
+            }
+        ],
+
+    };
+
+    return (
+        <div className='max-w-[1240px] mx-auto px-16 bg-black/0 mt-16 py-8 rounded-xl'>
+            <HighchartsReact highcharts={Highcharts} options={options} />
+        </div >
+    )
+}
+
+export default GraphBar;
