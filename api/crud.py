5188cfcce67b645f5e966940264bedf4eb68c2b7
jiwooshim <jiwooshim96@gmail.com>
2023-05-02 10:48:56 +0800

    add multiple model_id query endpoint and more specific product queries

--- api/crud.py
+++ api/crud.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql import text
 
 
 def get_all_prices(db: Session, skip: int = 0, limit: int = 100):
-    ## get prices for each listing on the enrtire database
+    # get prices for each listing on the enrtire database
     res = db.execute(text(f"""
         SELECT listing.*, product.model_name, product.color, product.capacity, product.current_  
         FROM (SELECT post_id, result_price, product_id FROM mydb.listing) as listing, 
@@ -17,15 +17,15 @@ def get_all_prices(db: Session, skip: int = 0, limit: int = 100):
     return df.set_index('post_id').to_dict(orient='index')
 
 
-def get_avg_prices(product_id: int, db: Session, skip: int = 0, limit: int = 100):
-    ## get prices for each listing on the enrtire database
+def get_avg_prices_single(product_id: int, db: Session, skip: int = 0, limit: int = 100):
+    # get prices for a single listing from the database
     if product_id > 0:
         res = db.execute(text(f"""
             SELECT listing.*, product.model_name, product.color, product.capacity, product.current_  
             FROM (SELECT post_id, postingdate, result_price, product_id FROM mydb.listing) as listing, 
             (SELECT id, model_name, color, capacity, current_ FROM mydb.product) as product 
             WHERE listing.product_id=product.id 
-            AND listing.product_id={product_id}
+            AND listing.product_id in ({product_id})
             AND product.current_=1;
         """))
     else:
@@ -38,7 +38,11 @@ def get_avg_prices(product_id: int, db: Session, skip: int = 0, limit: int = 100
         """))
 
     df = pd.DataFrame(list(res))
-    df['result_date_onlydate'] = pd.to_datetime(df['postingdate'].str[:19], format='%Y-%m-%dT%H:%M:%S').dt.date
+    if df.empty:
+        return None
+
+    df['result_date_onlydate'] = pd.to_datetime(
+        df['postingdate'].str[:19], format='%Y-%m-%dT%H:%M:%S').dt.date
 
     df_res = pd.DataFrame()
     df_group = df[['result_date_onlydate', 'product_id', 'result_price', 'model_name', 'capacity', 'color']]\
@@ -52,3 +56,94 @@ def get_avg_prices(product_id: int, db: Session, skip: int = 0, limit: int = 100
     return df_res.to_dict(orient='records')
     # return df_res.groupby('product_id')[['result_date_onlydate', 'price_avg', 'price_high', 'price_low']].apply(dict).to_dict()
 
+
+def get_avg_prices_multiple(product_id: str, db: Session, skip: int = 0, limit: int = 100):
+    # get prices of multiple product_id from the database
+    product_id_list = product_id.split(',')
+    if product_id != '':
+        res = db.execute(text(f"""
+            SELECT listing.*, product.model_name, product.color, product.capacity, product.current_  
+            FROM (SELECT post_id, postingdate, result_price, product_id FROM mydb.listing) as listing, 
+            (SELECT id, model_name, color, capacity, current_ FROM mydb.product) as product 
+            WHERE listing.product_id=product.id 
+            AND listing.product_id in ({product_id})
+            AND product.current_=1;
+        """))
+    else:
+        res = db.execute(text(f"""
+            SELECT listing.*, product.model_name, product.color, product.capacity, product.current_  
+            FROM (SELECT post_id, postingdate, result_price, product_id FROM mydb.listing) as listing, 
+            (SELECT id, model_name, color, capacity, current_ FROM mydb.product) as product 
+            WHERE listing.product_id=product.id 
+            AND product.current_=1;
+        """))
+
+    df = pd.DataFrame(list(res))
+    if df.empty:
+        return None
+
+    # Find unique columns out of "product_id" and "model_name"
+    product_id_unique = df['product_id'].unique()
+    model_name_unique = df['model_name'].unique()
+    color_unique = df['color'].unique()
+    capacity_unique = df['capacity'].unique()
+    group_id = ''
+    if product_id_unique.shape[0] == 1:
+        group_id = product_id_unique[0]
+    elif model_name_unique.shape[0] == 1:
+        if capacity_unique.shape[0] == 1:
+            group_id = f"{model_name_unique[0]} {capacity_unique[0]}"
+        elif color_unique.shape[0] == 1:
+            group_id = f"{model_name_unique[0]} {color_unique[0]}"
+        else:
+            group_id = model_name_unique[0]
+
+    df = df[['postingdate', 'result_price']]
+    df['group_id'] = group_id
+    df['result_date_onlydate'] = pd.to_datetime(
+        df['postingdate'].str[:19], format='%Y-%m-%dT%H:%M:%S').dt.date
+
+    df_res = pd.DataFrame()
+    df_group = df[['result_date_onlydate', 'group_id', 'result_price']]\
+        .astype({'group_id': str, 'result_price': float})\
+        .groupby(by=['group_id', 'result_date_onlydate'])
+    df_res.loc[:, 'price_avg'] = df_group.mean()
+    df_res.loc[:, 'price_high'] = df_group.max()
+    df_res.loc[:, 'price_low'] = df_group.min()
+    df_res = df_res.reset_index()
+
+    return df_res.to_dict(orient='records')
+    # return df_res.groupby('product_id')[['result_date_onlydate', 'price_avg', 'price_high', 'price_low']].apply(dict).to_dict()
+
+
+def get_product_details(product_id, model, capacity, color, current, db: Session, skip: int = 0, limit: int = 100):
+    product_query = ''
+    model_query = ''
+    capacity_query = ''
+    color_query = ''
+    current_query = ''
+    if product_id != '':
+        product_query = f"AND id in ({product_id})"
+    if model != '':
+        model_query = f"AND model_name='{model}'"
+    if capacity != '':
+        capacity_query = f"AND capacity='{capacity}'"
+    if color != '':
+        color_query = f"AND color='{color}'"
+    if current != 0:
+        current_query = f"AND current_='{current}'"
+
+    query = f"""
+        SELECT * FROM mydb.product 
+        WHERE 1
+        {product_query} 
+        {model_query} 
+        {capacity_query} 
+        {color_query} 
+        {current_query}
+    """
+
+    res = db.execute(text(query))
+    df = pd.DataFrame(list(res))
+
+    return df.to_dict(orient='records')
--- api/main.py
+++ api/main.py
@@ -4,9 +4,23 @@ import crud
 from database import SessionLocal, engine
 import uvicorn
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://newton.permonth.tech",
+    "http://permonth.tech"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def get_db():
@@ -17,7 +31,7 @@ def get_db():
         db.close()
 
 
-@app.get("/get-all/")
+@app.get("/api/get-all")
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     all_prices = crud.get_all_prices(db, skip=skip, limit=limit)
     # all_prices = list(all_prices)
@@ -26,12 +40,28 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return all_prices
 
 
-@app.get("/get-avg/")
-def read_users(product_id: int = 0, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    avg_prices = crud.get_avg_prices(product_id, db, skip=skip, limit=limit)
+@app.get("/api/get-avg-single")
+def read_users(product_id: str = '', skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    avg_prices = crud.get_avg_prices_single(product_id, db, skip=skip, limit=limit)
 
     return avg_prices
 
 
+@app.get("/api/get-avg-multiple")
+def read_users(product_id: str = '', skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    avg_prices = crud.get_avg_prices_multiple(
+        product_id, db, skip=skip, limit=limit)
+
+    return avg_prices
+
+
+@app.get("/api/get-products")
+def read_products(product_id: str = '', model: str = '', capacity: str = '', color: str = '', current: int = 0, skip: int = 0, db: Session = Depends(get_db)):
+    products = crud.get_product_details(
+        product_id, model, capacity, color, current, db)
+
+    return products
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
